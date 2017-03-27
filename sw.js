77dfc0240beb597f0d0fd31774dea099be5b1fc0
futureplayer <1981141934@qq.com>
2017-03-27 17:46:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ self.addEventListener('install', function(event) {
 	caches.open(CACHE_NAME)
 		.then(function(cache) {
 			console.log('Opened cache');
-			cache.addAll(urlsToCache.map(function(urlToPrefetch) {
+			return cache.addAll(urlsToCache.map(function(urlToPrefetch) {
 			  return new Request(urlToPrefetch, { mode: 'no-cors' });
 			})).then(function() {
 			  console.log('All resources have been fetched and cached.');
